433e3b3b6ad3305a9b4bccd9cccfeb159124a0ec
Ishtar <90619818+Ishtarcth@users.noreply.github.com>
2023-01-03 10:23:55 +0700

    reset

---
+++ index/js/script.js
@@ -0,0 +1,110 @@
+let navbar = document.querySelector('.navbar');
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () => {
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec => {
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+function loader() {
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
+
+
+// btn scrolltotop
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+btnScrollToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+
+const visitCount =
+{
+  show: function () {
+    var count = Number(localStorage.getItem('visitCount'));
+    if (count == null) {
+      count = 1;
+      localStorage.setItem('visitCount', count);
+    }
+    else {
+      count += 1;
+      localStorage.setItem('visitCount', count);
+    }
+    document.getElementById('visitCount').innerText = count;
+  },
+  setCount: function (count) {
+    localStorage.setItem('visitCount', count);
+    document.getElementById('visitCount').innerText = count;
+  },
+  delete: function () {
+    localStorage.removeItem('visitCount');
+  }
+}
+visitCount.show();
