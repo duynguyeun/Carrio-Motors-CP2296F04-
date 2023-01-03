6d95b5cd1995f9d27040392f95d35ee1d9f5f1dd
Ishtar <90619818+Ishtarcth@users.noreply.github.com>
2023-01-03 10:23:27 +0700

    reset

--- brand/bmw/js/script.js
+++
@@ -1,8 +0,0 @@
-const btnScrollToTop = document.querySelector("#btnScrollToTop");
-btnScrollToTop.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth"
-  });
-});
